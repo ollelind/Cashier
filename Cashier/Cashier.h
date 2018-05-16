cac63d6bc53c6f34c11cf1179069a51f4b285369
Olle Lind <mail@ollelind.se>
2018-05-16 16:12:49 +0200

    Initial Commit

---
+++ Cashier/Cashier.h
@@ -0,0 +1,19 @@
+//
+//  Cashier.h
+//  Cashier
+//
+//  Created by Olof Lind on 2018-05-16.
+//  Copyright © 2018 Olof Lind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cashier.
+FOUNDATION_EXPORT double CashierVersionNumber;
+
+//! Project version string for Cashier.
+FOUNDATION_EXPORT const unsigned char CashierVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cashier/PublicHeader.h>
+
+
